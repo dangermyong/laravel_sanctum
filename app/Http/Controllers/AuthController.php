73d58e1750d715016e5ce7fb1718b183127ca917
dangermyong <66460964+dangermyong@users.noreply.github.com>
2022-11-25 19:20:57 +0900

    finished

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -49,6 +49,10 @@ class AuthController extends Controller
 
   public function logout()
   {
-    return response()->json('This is my logout method');
+    Auth::user()->currentAccessToken()->delete();
+
+    return $this->success([
+      'message' => 'You logged out successfully and your token has been deleted'
+    ]);
   }
 }
