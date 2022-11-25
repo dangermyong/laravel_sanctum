204d302a20c1f751602c2daf776a9849d73a66a6
dangermyong <66460964+dangermyong@users.noreply.github.com>
2022-11-25 15:30:00 +0900

    before logout

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreTaskRequest;
 use App\Models\Task;
 use Illuminate\Http\Request;
+use App\Traits\HttpResponses;
 use App\Http\Resources\TaskResource;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\StoreTaskRequest;
 
 class TaskController extends Controller
 {
+  use HttpResponses;
   /**
    * Display a listing of the resource.
    *
@@ -50,7 +52,7 @@ class TaskController extends Controller
    */
   public function show(Task $task)
   {
-    //
+    return $this->isNotAuthorized($task) ? $this->isNotAuthorized($task) : new TaskResource($task);
   }
 
   /**
@@ -73,7 +75,13 @@ class TaskController extends Controller
    */
   public function update(Request $request, Task $task)
   {
-    //
+    if (Auth::user()->id !== $task->user_id) {
+      return $this->error('', 'You are not authorized to make this request', 403);
+    }
+
+    $task->update($request->all());
+
+    return new TaskResource($task);
   }
 
   /**
@@ -84,6 +92,13 @@ class TaskController extends Controller
    */
   public function destroy(Task $task)
   {
-    //
+    return $this->isNotAuthorized($task) ? $this->isNotAuthorized($task) : $task->delete();
+  }
+
+  private function isNotAuthorized($task)
+  {
+    if (Auth::user()->id !== $task->user_id) {
+      return $this->error('', 'You are not authorized to make this request', 403);
+    }
   }
 }
